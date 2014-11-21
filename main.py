133283cb465d7f6142035f45746e136f744c188e
guscl <correia.gustavol@gmail.com>
2014-11-21 22:22:55 +0000

    All the logic is now done and tested Signed-off-by: Gustavo Correia <correia.gustavol@gmail.com> Signed-off-by: Rodrigo Fay

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ import time
 import RPi.GPIO as GPIO
 
 #Outputs
-buzzerNobody = 26
-buzzerSomeone = 19
-engineOpening = 22
-engineClosing = 24
-pump = 23
+buzzerNobody = 29
+buzzerSomeone = 31
+engineOpening = 33
+engineClosing = 35
+pump = 37
 
 
 #Inputs
-xbeeButton = 8
 openedSensor = 3
 closedSensor = 5
 crashSensor = 7
-infraRed = 12
-start = 10
+xbeeButton = 11
+start = 13
+infraRed = 15
 
+GPIO.cleanup()
 
 #GPIO setUp
 GPIO.setmode(GPIO.BOARD)
@@ -40,28 +41,54 @@ GPIO.output(engineOpening, False)
 GPIO.output(engineClosing, False)
 GPIO.output(pump, False)
 
+
 def opening():
-	if GPIO.input(start) and GPIO.input(xbeeButton):				
+	print "opening()"
+	print GPIO.input(start) ," ", GPIO.input(xbeeButton)
+	GPIO.output(buzzerSomeone, 0)
+	GPIO.output(buzzerNobody, 0)
+	if GPIO.input(start) and GPIO.input(xbeeButton):
+		time.sleep(1)					
 		GPIO.output(pump, 0)
 		GPIO.output(engineOpening, 1)
+		print "engineOpening ON"
 		while True:
+			#print GPIO.input(openedSensor)
 			if GPIO.input(xbeeButton) == False:
+				time.sleep(0.1)	
 				if GPIO.input(infraRed):
+					time.sleep(0.1)	
 					GPIO.output(buzzerSomeone, 1)
+					print "buzzerSomeone ON"
 				else:
 					GPIO.output(buzzerNobody, 1)
-			if GPIO.input(closedSensor):
+					print "buzzerNobody ON"
+
+			if GPIO.input(xbeeButton) == True:
+				if GPIO.input(openedSensor) == False:
+					time.sleep(0.3)
+					GPIO.output(engineOpening, 0)
+					GPIO.output(buzzerSomeone, 0)
+					GPIO.output(buzzerNobody, 0)
+					print "ABRIU"
+					postOpening()
+					break
+				
+			if GPIO.input(openedSensor) == False:
+				time.sleep(0.3)
 				GPIO.output(engineOpening, 0)
 				GPIO.output(buzzerSomeone, 0)
 				GPIO.output(buzzerNobody, 0)
+				print "ABRIU"
 				postOpening()
 				break
 
-def forcedOpen():				
+def forcedOpen():
+	print "forcedOpen()"
 	GPIO.output(pump, 0)
 	GPIO.output(engineOpening, 1)
 	while True:
-		if GPIO.input(closedSensor):
+		if GPIO.input(openedSensor) == False:
 			GPIO.output(engineOpening, 0)
 			GPIO.output(buzzerSomeone, 0)
 			GPIO.output(buzzerNobody, 0)
@@ -70,29 +97,51 @@ def forcedOpen():
 
 #tem que por while na main
 def postOpening():
-	if not GPIO.input(xbeeButton):
-		if GPIO.input(infRared):
-			GPIO.output(buzzerSomeone, 1)
-		else:
-			time.sleep(10)
-			closing()
+	print "postOpening()"
+	while True:
+		if not GPIO.input(xbeeButton):
+			time.sleep(0.1)	
+			if GPIO.input(infraRed):
+				time.sleep(0.1)	
+				print "SAIU MAS TEM ALGUEM"
+				GPIO.output(buzzerSomeone, 1)
+				time.sleep(0.5)	
+				GPIO.output(buzzerSomeone, 0)
+				time.sleep(0.5)
+			else:
+				time.sleep(1)
+				closing()
 
 def closing():
+	print "closing()"
 	GPIO.output(engineClosing, 1)
+	print GPIO.input(crashSensor) ," ", GPIO.input(infraRed)
 	while True:
-		if GPIO.input(crashSensor) or GPIO.input(infraRed):
+		if not GPIO.input(crashSensor) or GPIO.input(infraRed):
+			time.sleep(0.1)	
 			#Someone has fallen
 			GPIO.output(buzzerSomeone, 1)
 			GPIO.output(engineClosing, 0)
-			forecedOpen()
+			print "CAIU ALGUEM"		
+			forcedOpen()
 			break
-		if GPIO.input(closedSensor):
-			GPIO.output(engineClosing, 1)
+		if GPIO.input(closedSensor) == False:
+			print "FECHOU"
+			GPIO.output(engineClosing, 0)
 			GPIO.output(pump, 1)
-			opening()
+			waiting()
+			break
+
+
+def waiting():
+	print "waiting"
+	while True:
+		time.sleep(0.1)	
+		opening()
+		
 
 if __name__ == '__main__':
-	opening()
+	waiting()	
 	GPIO.cleanup()
 	
 		
